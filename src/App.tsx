b8732faf4f33f0b8267be277a9c165bc35163107
Timileyin Adeoye <timiolamide1@gmail.com>
2026-05-12 17:45:19 +0100

    newly fix footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import Hero from './Components/Hero';
 import Navbar from './Components/Navbar';
 import Skills from './Components/Skills';
 import Projects from './Components/Projects';
-import Footer from './Components/Footer';
 import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+
 
 
 
